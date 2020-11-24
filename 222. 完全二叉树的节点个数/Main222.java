b271b430a11d147b901b3467910bdf5914de1c3e
feiyusheng <zhaochuang@jikugroup.com>
2020-11-24 19:32:57 +0800

    222. 很快啊，啪一下就提交了

---
+++ 222. 完全二叉树的节点个数/Main222.java
@@ -0,0 +1,25 @@
+import javax.swing.tree.TreeNode;
+
+public class Main222 {
+}
+
+class Solution222 {
+    public int countNodes(TreeNode222 root) {
+        if (root == null) {
+            return 0;
+        }
+        TreeNode leftNode;
+        int h = 0;
+        while ((leftNode = root.left) != null) {
+            h++;
+        }
+    }
+}
+
+
+class TreeNode222 {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode222(int x) { val = x; }
+  }
