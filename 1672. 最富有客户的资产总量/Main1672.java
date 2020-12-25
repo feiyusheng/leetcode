16a603c12a89fafe5b3dbb7bc570bf294415876a
feiyusheng <zhaochuang@jikugroup.com>
2020-12-25 13:38:31 +0800

    1672.双循环找最大值

--- 1672. 最富有客户的资产总量/Main1672.java
+++ 1672. 最富有客户的资产总量/Main1672.java
@@ -1,4 +1,44 @@
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+
 public class Main1672 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:/hh.zip");
+        File excel = new File("D:/外司价500条.xlsx");
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        OutputStream out = new FileOutputStream(file);
+
+        ZipOutputStream zipOutputStream = new ZipOutputStream(out);
+        try {
+            for (int i = 0; i < 5; i++) {
+                InputStream inputStream = new FileInputStream(excel);
+
+                ZipEntry z = new ZipEntry(i + "" + ".xlsx");
+
+                zipOutputStream.putNextEntry(z);
+                int ch;
+                while ((ch = inputStream.read()) != -1) {
+                    zipOutputStream.write(ch);
+                }
+//                zipOutputStream.closeEntry();
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            try {
+                zipOutputStream.close();
+            } catch (Throwable t1) {
+                t1.printStackTrace();
+            }
+            out.close();
+        }
+
+    }
 }
 
 class Solution1672 {
@@ -6,7 +46,7 @@ class Solution1672 {
         int maxAmount = 0;
         for (int[] oneAccounts : accounts) {
             int amount = 0;
-            for(int account : oneAccounts) {
+            for (int account : oneAccounts) {
                 amount += account;
             }
             if (amount > maxAmount) {
